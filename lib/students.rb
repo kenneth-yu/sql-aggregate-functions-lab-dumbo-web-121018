b8f5aba3453f4aed030200a9432f86fdfe830e49
Kenneth Yu <kyu22@buffalo.edu>
2018-12-17 16:44:02 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -11,11 +11,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "Select AVG"
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
