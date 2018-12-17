96e9dcef16ec8373a3631db65a7dd54b52db0464
Kenneth Yu <kyu22@buffalo.edu>
2018-12-17 16:42:43 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,13 +1,17 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT name,MAX(gpa) FROM students"
+  sql = "SELECT MAX(gpa) FROM students"
+  "SELECT name FROM students WHERE gpa = #{db.execute(sql)[0][0]}"
 end
 
 def lowest_student_gpa
+    sql = "SELECT MIN(gpa) FROM students"
+  "SELECT name FROM students WHERE gpa = #{db.execute(sql)[0][0]}"
 end
 
 def average_student_gpa
+  "Select AVG"
 end
 
 def total_tardies_for_all_students
